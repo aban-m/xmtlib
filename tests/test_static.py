07283603336f56f6064802dd86678fdba76fa456
aban-m <aban.salah.mahmoud@gmail.com>
2024-10-25 17:49:41 +0300

    Validated index parsing

--- tests/test_static.py
+++ tests/test_static.py
@@ -2,11 +2,34 @@ import pytest
 
 from ..xmt.recipes.static import parsing
 
-@pytest.mark.parametrize('index_string, value', [
-    ('1..4', [1, 2, 3, 4]),
-    ('3 .. 7; 1..  5', [1, 2, 3, 4, 5, 6, 7]),
-    ('4,  1, 2', [1, 2, 4])
-])
-def test_index_strings(index_string, value):
-    assert parsing.parse_index_string(index_string, max(value)) == value
+class TestIndexStrings:
+    @pytest.mark.parametrize('input, expected', [
+        ('1..4', [1, 2, 3, 4]),
+        ('8,9,10', [8, 9, 10]),
+        ('1', [1])
+    ])
+    def test_simple(self, input, expected):
+        assert parsing.parse_index_string(input, max(expected)) == expected
+    @pytest.mark.parametrize('input, expected', [
+        ('2,1,3,3,3', [1, 2, 3])
+    ])
+    def test_sort(self, input, expected):
+        assert parsing.parse_index_string(input, max(expected)) == expected
+    
+    @pytest.mark.parametrize('input, expected', [
+        ('1..4;5..8', [1, 2, 3, 4, 5, 6, 7, 8]),
+        ('1..1', [1])
+    ])
+    def test_complex(self, input, expected):
+        assert parsing.parse_index_string(input, max(expected) if expected else 1) == expected
 
+    @pytest.mark.parametrize('input, total_len', [
+        ('0..0', 1),
+        ('4..3', 5),
+        ('4..-1', 3),
+        ('4..3', 4)
+    ])
+    def test_vacuous(self, input, total_len):
+        # call parse_index_string. if an exception is raised, then the test passes.
+        with pytest.raises(Exception):
+            parsing.parse_index_string(input, total_len)
--- xmt/recipes/static/parsing.py
+++ xmt/recipes/static/parsing.py
@@ -9,19 +9,40 @@ def process_raw(L : list):
     ''' Returns a sorted, duplicate-free version of L'''
     return sorted(list(set(L)))
 
+def _process_index(i, total_len):
+    if i == 0: raise ValueError('Zero value is not allowed; indexing starts from 1.')
+    if i < 0: i = total_len + i
+    if i > total_len:
+        raise ValueError('Index must be <= total length.')
+    return i
+def _process_endpoints(start, end, total_len):
+    start = _process_index(start, total_len)
+    end = _process_index(end, total_len)
+
+    if start > end: raise ValueError('Start endpoint must be <= end endpoint.')
+    if end > total_len: raise ValueError('End endpoint must be <= total length.')
+
+    return start, end
+
 def parse_index_string(s, total_len):
     ''' Parse an index string, like 0..3;4,5 or 1,-1 '''
-    L = []
+    total_len += 1 # fixes the 0 case, and makes it inclusive
+
+    # recursive branch
     if ';' in s: 
         return process_raw(sum(
             [parse_index_string(subs.strip(), total_len) for subs in s.split(';')],
             start = []
         ))
     
+    # simple branch
     if '..' in s: # a range
         a, b = s.split('..')
-        return process_raw(range(int(a)%total_len, int(b)%total_len + 1))
+        a, b = _process_endpoints(int(a.strip()), int(b.strip()), total_len)
+        return [i for i in range(a, b+1)]
+    
     elif ',' in s: # a comma-separated list
-        return process_raw([int(c.strip())%total_len for c in s.split(',')])
+        return process_raw([_process_index(int(c.strip()), total_len) for c in s.split(',')])
     else: # a singleton
-        return [int(s)%total_len]
+        return [_process_index(int(s.strip()), total_len)]
+    
