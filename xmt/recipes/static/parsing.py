d7ccf25e7a62e754a3e9ae5251a223ff574d3282
aban-m <aban.salah.mahmoud@gmail.com>
2024-10-25 17:11:41 +0300

    Refactoring

--- xmt/recipes/static/parsing.py
+++ xmt/recipes/static/parsing.py
@@ -5,17 +5,23 @@ def parse_static_recipe(source):
         return parse_static_recipe(open(source, 'r', encoding='utf-8'))
     return yaml.safe_load(source)
 
+def process_raw(L : list):
+    ''' Returns a sorted, duplicate-free version of L'''
+    return sorted(list(set(L)))
+
 def parse_index_string(s, total_len):
     ''' Parse an index string, like 0..3;4,5 or 1,-1 '''
     L = []
-    if ';' in s: return sum(
-        [parse_index_string(subs.strip(), total_len) for subs in s.split(';')],
-        start = []
-    )
+    if ';' in s: 
+        return process_raw(sum(
+            [parse_index_string(subs.strip(), total_len) for subs in s.split(';')],
+            start = []
+        ))
     
-    if '..' in s:
+    if '..' in s: # a range
         a, b = s.split('..')
-        return list(range(int(a)%total_len, int(b)%total_len + 1))
-    elif ',' in s:
-        return [int(c)%total_len for c in s.split(',')]
-    else: return [int(s)%total_len]
+        return process_raw(range(int(a)%total_len, int(b)%total_len + 1))
+    elif ',' in s: # a comma-separated list
+        return process_raw([int(c.strip())%total_len for c in s.split(',')])
+    else: # a singleton
+        return [int(s)%total_len]
