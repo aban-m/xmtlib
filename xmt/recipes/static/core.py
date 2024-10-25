685b7b5155f828fa6c1583a31d7fb85be4837967
aban-m <aban.salah.mahmoud@gmail.com>
2024-10-25 17:11:14 +0300

    Added tags

--- xmt/recipes/static/core.py
+++ xmt/recipes/static/core.py
@@ -8,16 +8,18 @@ from collections import defaultdict
 
 try:
     from .utils import *
-    from .parse import parse_static_recipe, parse_index_string
+    from .parsing import parse_static_recipe, parse_index_string
     from ..base import Recipe
-    from ..models import Context, Spec, Environment
+    from ..models import Spec, Environment
+    from .processor import IndexCollection
     
 except ImportError:
     sys.path.append('..')
     from base import Recipe
-    from models import Context, Spec, Environment
+    from models import Spec, Environment
     from utils import *
     from parsing import parse_static_recipe, parse_index_string
+    from processor import IndexCollection
 
 
 # TODO:
@@ -25,52 +27,54 @@ except ImportError:
 #   2. A script that generates the YAML files.
 #   3. Graceful handling of inclusion
 #   4. Cleaning up the code in general (this is a resurrection)
-#   5. Better handling of the ordering of index strings
-#   6. __getitem__ magic.
+
 
 class StaticRecipe(Recipe):
     def __init__(self, spec: Spec, env: Environment):
         super().__init__(spec, env)
 
         self.content = []
-        self._content_len = None
 
         if not 'tags' in self.spec or not self.spec['tags']: self.spec['tags'] = {}
         if not 'annotations' in self.spec or not self.spec['annotations']: self.spec['annotations'] = {}
 
-        self.tags = defaultdict(list)
-        
+        self._tags = defaultdict(list)
+        self.tags = None
+
     def include(self, path):
+        ''' Handles the inclusion of a static recipe. '''
         subrecipe = StaticRecipe(parse_static_recipe(self.env.lookup(path)), self.env)
-        subrecipe.execute()
+        subrecipe.execute(compile_tags = False)
 
         precount = len(self.content)
         for chunk in subrecipe.content:
             self.content.append(chunk)
 
         for tag, what in subrecipe.tags.items():
-            self.tags[tag].extend([precount+i for i in what])
+            self._tags[tag].extend([precount+i for i in what])
         
     def process_includes(self):
+        ''' Iterates through the 'include' section, including every recipe. '''
         for item in self.spec['content']:
             if isinstance(item, dict):
                 if 'include' in item: self.include(item['include'])
                 continue
             self.content.append(defaultdict(str, {'content': item}))
-        self._content_len = len(self.content)
 
     def process_tags(self):
+        ''' Handles the 'tags' section. '''
         for tag, indstr in self.spec['tags'].items():
-            self.tags[tag] = parse_index_string(indstr, self._content_len)
+            self._tags[tag] = parse_index_string(indstr, len(self))
             
     def process_annotations(self):
+        ''' Handles the 'annotations' section. '''
         for ann, val in self.spec['annotations'].items():
             if isinstance(val, list):
                 cpos, rpos = 0, 0
                 while cpos < len(val):
                     pick = val[cpos]
                     if isinstance(pick, dict):
-                        if 'jump' in pick:
+                        if 'jump' in pick: # TODO: Handle jumping 
                             rpos = pick['jump']
                     else:
                         self.content[rpos][ann] = pick
@@ -81,10 +85,27 @@ class StaticRecipe(Recipe):
             else:
                 raise TypeError('Could not process annotation.')
 
-    def execute(self):
+    def compile_tags(self):
+        ''' Optimize the tags for fast access and filtering '''
+        for tag, indices in self._tags.items():
+            self.tags[tag] = IndexCollection(indices, len(self), tag)
+
+    def execute(self, compile_tags = True):
+        ''' Executes the recipe, making it ready for consumption. '''
         self.process_includes()
         self.process_tags()
+        if compile_tags: self.compile_tags()
         self.process_annotations()
+
+    def __getitem__(self, i): # TODO: Test
+        if isinstance(i, int) or isinstance(i, slice): return self.content[i]
+        elif isinstance(i, str): return self.tags[i]
+        elif isinstance(i, IndexCollection):
+            return [self[j] for j in i.indices]
+        else: raise TypeError(f'Slicing is not supported for type {type(i)}')
+    
+    def __len__(self): return len(self.content)
+    
 
 if __name__ == '__main__':
     import yaml
---
+++ xmt/recipes/static/processor.py
@@ -0,0 +1,80 @@
+from collections import defaultdict
+
+class IndexCollection:
+    ''' Essentially a list of indices, with fast set operations. '''
+    def __init__(self, indices, total_len, name = None):
+        self.name = name
+        self.indices = indices
+        self.indices_set = set(indices) # for faster inversion
+        self.total_len = total_len
+
+    def __len__(self): return len()
+    def __repr__(self): return f'IndexCollection({self.name}, with {len(self.indices)} indices)'
+    def __str__(self): return self.__repr__()
+
+    def __and__(self, other):
+        result = []
+        # use two pointers to find intersectiono
+        i, j = 0, 0
+        while i < len(self.indices) and j < len(other.indices):
+            if self.indices[i] == other.indices[j]:
+                result.append(self.indices[i])
+                i += 1
+                j += 1
+            elif self.indices[i] < other.indices[j]:
+                i += 1
+            else:
+                j += 1
+        return IndexCollection(result, min(self.total_len, other.total_len), 
+                               f'({self.name} & {other.name})' if self.name and other.name else None)
+
+    def __or__(self, other):
+        result = []
+        # use two pointers to find union
+        i, j = 0, 0
+        while i < len(self.indices) and j < len(other.indices):
+            if self.indices[i] == other.indices[j]:
+                result.append(self.indices[i])
+                i += 1
+                j += 1
+            elif self.indices[i] < other.indices[j]:
+                result.append(self.indices[i])
+                i += 1
+            else:
+                result.append(other.indices[j])
+                j += 1
+        return IndexCollection(result, min(self.total_len, other.total_len),
+                               f'({self.name} | {other.name})' if self.name and other.name else None)
+    
+    def __sub__(self, other):
+        result = []
+        # use two pointers to find difference
+        i, j = 0, 0
+        while i < len(self.indices) and j < len(other.indices):
+            if self.indices[i] == other.indices[j]:
+                i += 1
+                j += 1
+            elif self.indices[i] < other.indices[j]:
+                result.append(self.indices[i])
+                i += 1
+            else:
+                j += 1
+        return IndexCollection(result, min(self.total_len, other.total_len),
+                               f'({self.name} - {other.name})' if self.name and other.name else None)
+    
+    # define the minus sign to be a complement
+    def __invert__(self):
+        result = []
+        for i in range(self.total_len):
+            if i not in self.indices_set:
+                result.append(i)
+        return IndexCollection(result, self.total_len, f'~{self.name}')
+    
+
+    def __eq__(self, other):
+        return self.indices == other.indices
+    
+    def __hash__(self):
+        return hash((tuple(self.indices), self.total_len, self.name))
+    
+        
