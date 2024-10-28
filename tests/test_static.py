62ab5d00e8f80e3400a2eddb4c46ae306b9eefd3
aban-m <aban.salah.mahmoud@gmail.com>
2024-10-28 20:29:42 +0300

    Refactoring

--- tests/test_static.py
+++ tests/test_static.py
@@ -1,3 +1,4 @@
+from calendar import c
 import pytest
 
 import sys
@@ -5,70 +6,37 @@ from copy import deepcopy
 
 
 from ..xmt.recipes.static import parsing
-from ..xmt.recipes.static.core import StaticRecipe
-from ..xmt.recipes.storage import CyclicDependencyException, MemoryStorage
-
-@pytest.fixture
-def basic_recipe():
-    spec = {
-            'metadata': {
-                'name': 'test',
-                'type': 'static',
-                'content': 'hello world',
-                'id': 'test-basic'
-            },
-            'content': ['line1', 'line2', 'line3'],
-            'tags': {
-                'all': '1..3',
-                'first': '1',
-                'last_two': '2,3'
-            },
-            'annotations': {
-                'comment': ['first comment', 'second comment', 'third comment']
-            }
-    }
-    env = MemoryStorage()
-    env['basic'] = spec
-    recipe = StaticRecipe(spec, env)
-    return recipe
-
-
-@pytest.fixture
-def cyclic_dependency_recipe(request):
-# get the value of basic_recipe
-    basic = request.getfixturevalue('basic_recipe')
-    basic.spec['content'].append({'include': 'basic'})
-    return basic
 
 class TestTags:
-    def test_simple(self, basic_recipe):
-        basic_recipe.execute()
+    def test_simple(self, sta):
+        sta.execute()
         # simple tests
-        assert basic_recipe._tags['all'] == [1, 2, 3]
-        assert basic_recipe._tags['first'] == [1]
-        assert basic_recipe._tags['last_two'] == [2, 3]
+        assert sta._tags['all'] == [1, 2, 3]
+        assert sta._tags['first'] == [1]
+        assert sta._tags['last_two'] == [2, 3]
         # complex tests
-        assert (~basic_recipe['all']).indices == []
-        assert (basic_recipe['first'] | basic_recipe['last_two']).indices == [1, 2, 3]
+        assert (~sta['all']).indices == []
+        assert (sta['first'] | sta['last_two']).indices == [1, 2, 3]
 
 
 class TestInclude:
     # mark as expected failure
-    def test_cyclic_dependency(self, cyclic_dependency_recipe):
+    def test_cyclic_dependency(self, sta):
         rec_lim = sys.getrecursionlimit()
         sys.setrecursionlimit(100)
         try:
-            cyclic_dependency_recipe.execute()
-        except CyclicDependencyException:
-            pass  # expected
+            sta.spec['content'].append({'include': 'basic'})
+            sta.execute()
         except RecursionError:
             pytest.fail('Cyclic dependency not caught.')
+        except Exception:
+            pass # succeded.
         finally:
             sys.setrecursionlimit(rec_lim)  # reset the recursion limit.
 
-    def test_attachments_integration(self, basic_recipe):
+    def test_attachments_integration(self, sta):
         # set up the environment
-        clone_spec = deepcopy(basic_recipe.spec)
+        clone_spec = deepcopy(sta.spec)
         clone_spec['metadata']['id'] = 'dep'
         clone_spec['metadata']['name'] = 'dep'
         clone_spec['annotations'] = {
@@ -79,16 +47,16 @@ class TestInclude:
         }
 
         # add the dependency
-        basic_recipe.spec['content'].append({'include': 'dep'})
-        basic_recipe.env['dep'] = clone_spec        # enough to initiate a clone
-        basic_recipe.execute()
+        sta.spec['content'].append({'include': 'dep'})
+        sta.env['dep'] = clone_spec        # enough to initiate a clone
+        sta.execute()
 
         # check the tags
-        assert basic_recipe._tags['all'] == list(range(1, 7))
-        assert basic_recipe._tags['first'] == [1, 4]
+        assert sta._tags['all'] == list(range(1, 7))
+        assert sta._tags['first'] == [1, 4]
     
         # check the annotations: the attached ones should have extra comments.
-        assert all('extra-comment' in basic_recipe[i] for i in [3, 4, 5])
+        assert all('extra-comment' in sta[i] for i in [3, 4, 5])
 
 
 class TestIndexStrings:
